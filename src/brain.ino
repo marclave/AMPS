f7c0026cd8781b4ab17db9fa8f8f53d7122c4207
marclave <laventure.marc@gmail.com>
2015-07-13 19:25:39 -0700

    First succesfull finish line test, with junctions

--- src/brain.ino
+++ src/brain.ino
@@ -6,22 +6,26 @@
 #define RIGHT_SENSOR   0
 #define MIDDLE_SENSOR  1
 #define LEFT_SENSOR    2
-#define READY_TO_DRIVE 12
 
-#define THRESHOLD 900
+#define READY_TO_DRIVE 12
+#define DELETE_EEPROM  13   
+
+#define THRESHOLD 930
+#define THRESHOLD_MIDDLE 840
 
 #define LEFT_DIRECTION  0x4c
 #define RIGHT_DIRECTION 0x52
 #define UP_DIRECTION    0x55
 #define U_TURN          0x54
 
-#define LEFT_MOTOR_SPEED  105
-#define RIGHT_MOTOR_SPEED 100
+#define LEFT_MOTOR_SPEED  155
+#define RIGHT_MOTOR_SPEED 150
 
 #include <EEPROM.h>
 
 unsigned char SHORTESTPATH[512];
 uint16_t PATH_ELEMENT = 0;
+bool checkedFinish = false;
 
 bool isEEPROMempty(void)
 {
@@ -32,10 +36,15 @@ bool isEEPROMempty(void)
     eepromValue = EEPROM.read(eepromAddress);
     if (((eepromValue & LEFT_DIRECTION) != 0x00) || ((eepromValue & RIGHT_DIRECTION) != 0x00) || ((eepromValue & UP_DIRECTION) != 0x00) || ((eepromValue & U_TURN) != 0x00))
     {
-      return false; // This means EEPROM is not empty
+      return true; // This means EEPROM is empty
+      
+      Serial.println("EEPROM is empty");
     }    
   }
-  return true; // This means we have our path
+  
+  Serial.println("EEPROM is not empty");
+  return false; // This means we have our path
+  
 }
 
 void readEEPROM(void)
@@ -44,6 +53,8 @@ void readEEPROM(void)
   {
      SHORTESTPATH[eepromAddress] = EEPROM.read(eepromAddress);
   } 
+  
+  Serial.println("EEPROM Read from"); 
 }
 
 void clearEEPROM(void)
@@ -52,6 +63,8 @@ void clearEEPROM(void)
   {
      EEPROM.write(eepromAddress, '\0');
   }
+  
+  Serial.println("EEPROM Cleared"); 
 }
 
 void writeShortestPath(void)
@@ -60,6 +73,8 @@ void writeShortestPath(void)
   {
      EEPROM.write(eepromAddress, SHORTESTPATH[eepromAddress]);
   }
+  
+  Serial.println("Shortest path written to EEPROM!"); 
 }
 
 
@@ -70,7 +85,7 @@ void setup() {
   while(!digitalRead(READY_TO_DRIVE))
   {
     /**
-    if (deleteEEPROM)
+    if (digitalRead(DELETE_EEPROM))
     {
       clearEEPROM();
     }
@@ -93,7 +108,8 @@ void setup() {
       Serial.print(sensRight);
       Serial.println("");
   }
-  
+  Serial.println("Driving");
+  delay(1000);
   /**
   if (isEEPROMempty)
   {
@@ -106,7 +122,7 @@ void setup() {
 
 void performUturn(void)
 {
- while(analogRead(MIDDLE_SENSOR) < THRESHOLD)
+ while(analogRead(MIDDLE_SENSOR) < THRESHOLD_MIDDLE)
   {
     analogWrite(LEFT_MOTOR_FORWARD, LEFT_MOTOR_SPEED);
     analogWrite(LEFT_MOTOR_BACKWARD, 0);
@@ -115,15 +131,18 @@ void performUturn(void)
   }
   
   Serial.println("TURNING AROUND");
-  //SHORTESTPATH[PATH_ELEMENT] = LEFT_DIRECTION;
+  checkedFinish = true;
+  //SHORTESTPATH[PATH_ELEMENT] = U_TURN;
   //PATH_ELEMENT++;
 }
 
 void turnRight(void)
 {
-  delay(100); // Just for positioning with sensors in front
-  while(analogRead(MIDDLE_SENSOR) < THRESHOLD)
+  //delay(50); // Just for positioning with sensors in front
+  while(analogRead(MIDDLE_SENSOR) < THRESHOLD_MIDDLE)
   {
+    //int mid = analogRead(MIDDLE_SENSOR);
+//Serial.println(mid);
     analogWrite(LEFT_MOTOR_FORWARD, LEFT_MOTOR_SPEED);
     analogWrite(LEFT_MOTOR_BACKWARD, 0);
     analogWrite(RIGHT_MOTOR_FORWARD, 0);
@@ -131,25 +150,30 @@ void turnRight(void)
   }
   
   Serial.println("TURNING RIGHT");
+  checkedFinish = true;
   //SHORTESTPATH[PATH_ELEMENT] = RIGHT_DIRECTION;
   //PATH_ELEMENT++;
 }
 
-void debugShortestPath(void)
+void stopForTesting(void)
 {
-  for (uint16_t elem; elem < 512; elem++)
-  {
-     Serial.print(SHORTESTPATH[elem]);
-     Serial.print(" ");
-  } 
-  Serial.println("");
+    analogWrite(LEFT_MOTOR_FORWARD, 0);
+    analogWrite(LEFT_MOTOR_BACKWARD, 0);
+    analogWrite(RIGHT_MOTOR_FORWARD, 0);
+    analogWrite(RIGHT_MOTOR_BACKWARD, 0);
+    writeShortestPath();
+    while(1)
+    {
+       delay(10000);
+       readEEPROM();
+    }
 }
 
 void turnLeft(void)
 {
-  delay(100); // Just for positioning with sensors in front
+  //delay(50); // Just for positioning with sensors in front
 
-  while(analogRead(MIDDLE_SENSOR) < THRESHOLD)
+  while(analogRead(MIDDLE_SENSOR) < THRESHOLD_MIDDLE)
   {
       analogWrite(LEFT_MOTOR_FORWARD, 0);
       analogWrite(LEFT_MOTOR_BACKWARD, LEFT_MOTOR_SPEED);
@@ -157,6 +181,51 @@ void turnLeft(void)
       analogWrite(RIGHT_MOTOR_BACKWARD, 0);
   }
     Serial.println("TURNING LEFT");
+    checkedFinish = true;
+}
+
+void finished(void)
+{
+  while(1)
+  {
+    analogWrite(LEFT_MOTOR_FORWARD, 0);
+    analogWrite(LEFT_MOTOR_BACKWARD, 0);
+    analogWrite(RIGHT_MOTOR_FORWARD, 0);
+    analogWrite(RIGHT_MOTOR_BACKWARD, 0);
+  }
+}
+
+bool finishLine(void)
+{
+  analogWrite(LEFT_MOTOR_FORWARD, LEFT_MOTOR_SPEED);
+  analogWrite(LEFT_MOTOR_BACKWARD, 0);
+  analogWrite(RIGHT_MOTOR_FORWARD, RIGHT_MOTOR_SPEED);
+  analogWrite(RIGHT_MOTOR_BACKWARD, 0);
+  delay(100);
+  
+  int sensMid = analogRead(MIDDLE_SENSOR);
+  int sensRight = analogRead(RIGHT_SENSOR);
+  int sensLeft = analogRead(LEFT_SENSOR);
+  
+  if ((sensRight > THRESHOLD) && (sensMid > THRESHOLD_MIDDLE) && (sensLeft > THRESHOLD))
+  {
+     Serial.println("FINISH");
+     return true;
+  }
+  else
+  {
+     return false; 
+  }
+}
+
+void moveBack(void)
+{
+  analogWrite(LEFT_MOTOR_FORWARD, 0);
+  analogWrite(LEFT_MOTOR_BACKWARD, LEFT_MOTOR_SPEED);
+  analogWrite(RIGHT_MOTOR_FORWARD, 0);
+  analogWrite(RIGHT_MOTOR_BACKWARD, RIGHT_MOTOR_SPEED);
+  delay(100);
+  
 }
 
 void loop()
@@ -165,24 +234,38 @@ void loop()
   int sensRight = analogRead(RIGHT_SENSOR);
   int sensLeft = analogRead(LEFT_SENSOR);
   
+  /**
   Serial.print(sensLeft);
   Serial.print(" ");
   Serial.print(sensMid);
   Serial.print(" ");
   Serial.print(sensRight);
   Serial.println("");
+  **/
   
-  if (sensRight > THRESHOLD)
+  if ((sensRight > THRESHOLD) && (sensMid > THRESHOLD_MIDDLE) && (sensLeft > THRESHOLD) && (checkedFinish == false))
+  {
+     if(finishLine())
+     {
+       finished();
+     } 
+     else
+     {
+       moveBack();
+     }
+  }
+  else if (sensRight > THRESHOLD)
   {
    turnRight(); 
   }
-  else if ((sensLeft < THRESHOLD) && (sensMid > THRESHOLD))
+  else if ((sensLeft < THRESHOLD) && (sensMid > THRESHOLD_MIDDLE))
   {
     // This is just go straight
     analogWrite(LEFT_MOTOR_FORWARD, LEFT_MOTOR_SPEED);
     analogWrite(LEFT_MOTOR_BACKWARD, 0);
     analogWrite(RIGHT_MOTOR_FORWARD, RIGHT_MOTOR_SPEED);
     analogWrite(RIGHT_MOTOR_BACKWARD, 0);
+    checkedFinish = true;
   }
   else if (sensLeft > THRESHOLD)
   { 
@@ -198,5 +281,9 @@ void loop()
   analogWrite(RIGHT_MOTOR_FORWARD, RIGHT_MOTOR_SPEED);
   analogWrite(RIGHT_MOTOR_BACKWARD, 0);
   
-  //debugShortestPath();
+  if (digitalRead(READY_TO_DRIVE))
+  {
+     stopForTesting();   
+  }
+ 
 }
